da8c56a63beb5c374cc3ac24c65b0b42c00c82cf
Jesse-Tingle <jtingle0@gmail.com>
2020-10-11 20:23:06 -0400

    adds solution for ex1 and passes all tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,10 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    weight_cache = {}
 
+    for i in range(length):
+        complement = limit - weights[i]
+
+        if complement in weight_cache:
+            return (i, weight_cache[complement])
+        weight_cache[weights[i]] = i
     return None
